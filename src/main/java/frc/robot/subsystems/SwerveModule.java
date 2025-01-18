45a676e0b95ba231f424bf13bc4f82e18d113811
Phred <steveking16@outlook.com>
2025-01-18 13:34:10 -0800

    Climb done

---
+++ src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -0,0 +1,133 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkAbsoluteEncoder;
+import com.revrobotics.spark.SparkClosedLoopController;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+
+import java.io.ObjectInputFilter.Config;
+
+import com.revrobotics.AbsoluteEncoder;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import frc.robot.Configs;
+import frc.robot.Constants;
+import frc.robot.Constants.ModuleConstants;
+
+/**
+ * Constructs a MAXSwerveModule and configures the driving and turning motor,
+ * encoder, and PID controller. This configuration is specific to the REV
+ * MAXSwerve Module built with NEOs, SPARKS MAX, and a Through Bore
+ * Encoder.
+ */
+public class SwerveModule {
+
+  private final SparkMax driveSparkMax;
+  private final SparkMax turningSparkMax;
+
+  private final RelativeEncoder driveEncoder;
+  private final AbsoluteEncoder turningEncoder;
+
+  private final SparkClosedLoopController drivingClosedLoopController;
+  private final SparkClosedLoopController turningClosedLoopController;
+
+  private double chassisAngularOffset = 0;
+  private SwerveModuleState moduleDesiredState = new SwerveModuleState(0.0, new Rotation2d());
+
+  public SwerveModule(
+      int driveMotorCANId,
+      int turningMotorCANId,
+      double moduleAngularOffset) {
+
+    // Driving SparkMax
+    driveSparkMax = new SparkMax(driveMotorCANId, MotorType.kBrushless);
+    driveEncoder = driveSparkMax.getEncoder();
+    drivingClosedLoopController = driveSparkMax.getClosedLoopController();
+
+    driveSparkMax.configure(Configs.MAXSwerveModule.drivingConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
+    // Turning SparkMax
+    turningSparkMax = new SparkMax(turningMotorCANId, MotorType.kBrushless);
+    turningEncoder = turningSparkMax.getAbsoluteEncoder();
+    turningClosedLoopController = turningSparkMax.getClosedLoopController();
+
+    turningSparkMax.configure(Configs.MAXSwerveModule.turningConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
+    chassisAngularOffset = moduleAngularOffset;
+    moduleDesiredState.angle = new Rotation2d(turningEncoder.getPosition());
+    driveEncoder.setPosition(0);
+
+  }
+
+  public double getDrivePosition() {
+    return -driveEncoder.getPosition();
+  }
+
+  /**
+   * Returns current turn position in range -pi to pi
+   */
+  // Before it was
+  // return turningSparkMax.getEncoder().getPosition();
+
+  public double getTurningPosition() {
+    if (turningEncoder.getPosition() >= 0) {
+      return turningEncoder.getPosition();
+
+    } else {
+      return Constants.k2pi + turningEncoder.getPosition();
+    }
+    // ModuleConstants.kTurningMotorRotationPerSteerRotation;
+  }
+
+  public SwerveModulePosition getPosition() {
+    return new SwerveModulePosition(
+        driveEncoder.getPosition(),
+        new Rotation2d(turningEncoder.getPosition() - chassisAngularOffset));
+  }
+
+  public double getDriveVelocity() {
+    return driveEncoder.getVelocity();
+  }
+
+  public double getTurningVelocity() {
+    return turningEncoder.getVelocity();
+  }
+
+  public void resetEncoders() {
+    driveEncoder.setPosition(0);
+  }
+
+  public SwerveModuleState getState() {
+    return new SwerveModuleState(getDriveVelocity(),
+        new Rotation2d(turningEncoder.getPosition() - chassisAngularOffset));
+  }
+
+  public void setDesiredState(SwerveModuleState desiredState) {
+    SwerveModuleState correctedDesiredState = new SwerveModuleState();
+    correctedDesiredState.speedMetersPerSecond = desiredState.speedMetersPerSecond;
+    correctedDesiredState.angle = desiredState.angle.plus(Rotation2d.fromRadians(chassisAngularOffset));
+    // changed fromRadians to fromDegrees
+
+     // Optimize the reference state to avoid spinning further than 90 degrees.
+     correctedDesiredState.optimize(new Rotation2d(turningEncoder.getPosition()));
+
+    driveSparkMax.getClosedLoopController().setReference(correctedDesiredState.speedMetersPerSecond,
+        SparkMax.ControlType.kVelocity);
+    turningSparkMax.getClosedLoopController().setReference(correctedDesiredState.angle.getRadians(),
+        SparkMax.ControlType.kPosition);
+    
+    moduleDesiredState = desiredState;
+  }
+
+  public void stop() {
+    driveSparkMax.set(0);
+    turningSparkMax.set(0);
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -0,0 +1,163 @@
+package frc.robot.subsystems;
+
+//import com.kauailabs.navx.frc.AHRS;
+import com.studica.frc.AHRS;
+import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.DriveConstants;
+import frc.robot.controls.LogitechPro;
+
+public class SwerveSubsystem extends SubsystemBase {
+  private final SwerveModule frontLeft = new SwerveModule(
+      DriveConstants.kFrontLeftDriveMotorCANId,
+      DriveConstants.kFrontLeftTurningMotorCANId,
+      DriveConstants.kFrontLeftModuleChassisAngularOffset);
+
+  private final SwerveModule frontRight = new SwerveModule(
+      DriveConstants.kFrontRightDriveMotorCANId,
+      DriveConstants.kFrontRightTurningMotorCANId,
+      DriveConstants.kFrontRightModuleChassisAngularOffset);
+
+  public final SwerveModule backLeft = new SwerveModule(
+      DriveConstants.kBackLeftDriveMotorCANId,
+      DriveConstants.kBackLeftTurningMotorCANId,
+      DriveConstants.kBackLeftModuleChassisAngularOffset);
+
+  private final SwerveModule backRight = new SwerveModule(
+      DriveConstants.kBackRightDriveMotorCANId,
+      DriveConstants.kBackRightTurningMotorCANId,
+      DriveConstants.kBackRightModuleChassisAngularOffset);
+
+  private final AHRS gyro = new AHRS(AHRS.NavXComType.kMXP_SPI);
+  // private final SimDeviceSim simGyro = new SimDeviceSim();
+  private final Field2d m_field = new Field2d();
+
+  private final LogitechPro m_joyStick;
+
+  // private final SwerveDriveOdometry odometer = new
+  // SwerveDriveOdometry(DriveConstants.kDriveKinematics,
+  // new Rotation2d(), getModulePositions());
+
+  SwerveDriveOdometry odometer = new SwerveDriveOdometry(
+      DriveConstants.kDriveKinematics, gyro.getRotation2d(),
+      getModulePositions(), new Pose2d(0.0, 0.0, new Rotation2d()));
+
+  public SwerveModulePosition[] getModulePositions() {
+    return new SwerveModulePosition[] {
+        frontLeft.getPosition(),
+        frontRight.getPosition(),
+        backLeft.getPosition(),
+        backRight.getPosition()
+    };
+  }
+
+  public SwerveSubsystem(LogitechPro joystick) {
+    m_joyStick = joystick;
+    SmartDashboard.putData("Field", m_field);
+    new Thread(() -> {
+      try {
+        Thread.sleep(1000);
+        zeroHeading();
+        initModules();
+      } catch (Exception e) {
+      }
+    }).start();
+  }
+
+  public void zeroHeading() {
+    // gyro.zeroYaw();
+    gyro.reset();
+    frontLeft.resetEncoders();
+    frontRight.resetEncoders();
+    backLeft.resetEncoders();
+    backRight.resetEncoders();
+
+  }
+
+  public double getHeading() {
+    if (gyro.isMagnetometerCalibrated()) {
+      // We will only get valid fused headings if the magnetometer is calibrated
+      return gyro.getFusedHeading();
+    }
+
+    // We have to invert the angle of the NavX so that rotating the robot
+    // counter-clockwise makes the angle increase.
+    // if (-gyro.getYaw() >= 0) {
+    return gyro.getYaw();
+
+    // } else {
+    // return 360 + -gyro.getYaw();
+    // }
+  }
+
+  public Rotation2d getRotation2d() {
+    return Rotation2d.fromDegrees(getHeading());
+  }
+
+  public Pose2d getPose() {
+    return odometer.getPoseMeters();
+  }
+
+  public void resetOdometry(Pose2d pose) {
+    odometer.resetPosition(getRotation2d(), getModulePositions(), getPose());
+  }
+
+  // -------------------------------------------------
+  // ------------- Update Dashboard ----------------
+  @Override
+  public void periodic() {
+    odometer.update(getRotation2d(), getModulePositions());
+
+    SmartDashboard.putNumber("throttle", m_joyStick.getThrottl());
+
+    SmartDashboard.putString("Robot Heading (Rotation2d)", gyro.getRotation2d().toString());
+    SmartDashboard.putNumber("Robot Heading (Degrees)", getHeading());
+    SmartDashboard.putNumber("Front Left Turning Position", frontLeft.getTurningPosition() * (180 / Math.PI));
+    SmartDashboard.putNumber("Front Right Turning Position", frontRight.getTurningPosition() * (180 / Math.PI));
+    SmartDashboard.putNumber("Back Left Turning Position", backLeft.getTurningPosition() * (180 / Math.PI));
+    SmartDashboard.putNumber("Back Right Turning Position", backRight.getTurningPosition() * (180 / Math.PI));
+
+    SmartDashboard.putString("Robot Location", getPose().getTranslation().toString());
+
+    SmartDashboard.putBoolean("Should we blame Hardware/Electrical?", true);
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    odometer.update(new Rotation2d(-Math.PI), getModulePositions());
+    m_field.setRobotPose(odometer.getPoseMeters());
+  }
+
+  public void initModules() {
+    SwerveModuleState state = new SwerveModuleState(0, new Rotation2d());
+    frontLeft.setDesiredState(state);
+    frontRight.setDesiredState(state);
+    backLeft.setDesiredState(state);
+    backRight.setDesiredState(state);
+  }
+
+  public void stopModules() {
+    frontLeft.stop();
+    frontRight.stop();
+    backLeft.stop();
+    backRight.stop();
+  }
+  // state.angle.getRadians());
+
+  public void setModuleStates(SwerveModuleState[] desiredStates) {
+    SwerveDriveKinematics.desaturateWheelSpeeds(desiredStates, DriveConstants.kPhysicalMaxSpeedMetersPerSecond);
+    frontLeft.setDesiredState(desiredStates[0]);
+    frontRight.setDesiredState(desiredStates[1]);
+    backLeft.setDesiredState(desiredStates[2]);
+    backRight.setDesiredState(desiredStates[3]);
+  }
+
+}
